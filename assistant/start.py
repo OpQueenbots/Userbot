3dc3b88b0920429c1a4b6d8c2406acfd8e09a3be
OpQueenbots <120937975+OpQueenbots@users.noreply.github.com>
2023-05-31 18:51:34 +0530

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -29,7 +29,7 @@ if Owner_info_msg is None:
 
 **Message Forwards** - {udB.get_key("PMBOT")}
 
-**Ultroid [v{ultroid_version}](https://github.com/TeamUltroid/Ultroid), powered by @TeamUltroid**
+**Userbot [v{ultroid_version}](https://t.meRedWine_MusicWorld), powered by @Red_Wine_Op**
 """
 
 
@@ -68,7 +68,7 @@ async def own(event):
         mention=event.sender.mention, me=inline_mention(ultroid_bot.me)
     )
     if custom_info:
-        msg += "\n\n• Powered by **@TeamUltroid**"
+        msg += "\n\n• Powered by **@RedWine_MusicWorld**"
     await event.edit(
         msg,
         buttons=[Button.inline("Close", data="closeit")],
